53f78b95503d25dccdd59ae0aa7890808a6142b8
Clement <clement@umr3664-pas-1ss-109b-6593.curie.fr>
2018-01-23 15:43:05 +0100

    1

--- src/download.py
+++ src/download.py
@@ -31,7 +31,8 @@ try :
 	import ConfigParser
 except ImportError:
 	import configparser as ConfigParser
-
+class Ordinateur():
+	
 class Download():
 
 	def __init__(self,  link_genome_homo_sapiens, link_protein_feature, link_repeat_consensus, link_repeat_feature, number_study):
@@ -161,7 +162,7 @@ class Download():
 		# exit(0)
 		try:
 
-			subprocess.check_output(['bash', '-c', "wget -nc "+"'"+"https://www.ebi.ac.uk/ena/data/warehouse/filereport?accession=" +self.number_study+ "&result=read_run&fields=study_accession,secondary_study_accession,sample_accession,secondary_sample_accession,experiment_accession,run_accession,submission_accession,tax_id,scientific_name,instrument_platform,instrument_model,library_name,nominal_length,library_layout,library_strategy,library_source,library_selection,read_count,base_count,center_name,first_public,last_updated,experiment_title,study_title,study_alias,experiment_alias,run_alias,fastq_bytes,fastq_md5,fastq_ftp,fastq_aspera,fastq_galaxy,submitted_bytes,submitted_md5,submitted_ftp,submitted_aspera,submitted_galaxy,submitted_format,sra_bytes,sra_md5,sra_ftp,sra_aspera,sra_galaxy,cram_index_ftp,cram_index_aspera,cram_index_galaxy,sample_alias,broker_name,sample_title,nominal_sdev,first_created&download=txt"+"'" + " --output-document=" + os.path.join(self.path_study, self.number_study) + ".txt &>/dev/null"])
+			subprocess.check_output(['bash', '-c', "wget -nc "+"'"+"https://www.ebi.ac.uk/ena/data/warehouse/filereport?accession=" + self.number_study+ "&result=read_run&fields=study_accession,secondary_study_accession,sample_accession,secondary_sample_accession,experiment_accession,run_accession,submission_accession,tax_id,scientific_name,instrument_platform,instrument_model,library_name,nominal_length,library_layout,library_strategy,library_source,library_selection,read_count,base_count,center_name,first_public,last_updated,experiment_title,study_title,study_alias,experiment_alias,run_alias,fastq_bytes,fastq_md5,fastq_ftp,fastq_aspera,fastq_galaxy,submitted_bytes,submitted_md5,submitted_ftp,submitted_aspera,submitted_galaxy,submitted_format,sra_bytes,sra_md5,sra_ftp,sra_aspera,sra_galaxy,cram_index_ftp,cram_index_aspera,cram_index_galaxy,sample_alias,broker_name,sample_title,nominal_sdev,first_created&download=txt"+"'" + " --output-document=" + os.path.join(self.path_study, self.number_study) + ".txt &>/dev/null"])
 
 		except subprocess.CalledProcessError as error:
 
@@ -169,11 +170,39 @@ class Download():
 			self.logger.warning("Exception at the line : {}".format(sys.exc_info()[-1].tb_lineno))
 			self.logger.warning(sys.exc_info())
 
-	def get_study(self):
+	def get_all_study(self):
 		
 		filename = str(os.path.join(self.path_study, self.number_study) + ".txt")
 		df = pd.read_csv(filename, header=0, sep='\t', dtype=str)
-		print df
+		headers = df.columns.values.tolist()
+		index_sample_title = headers.index('sample_title')
+		index_fastq_ftp = headers.index('fastq_ftp')
+		index_read_count = headers.index('read_count')
+
+		df = df.iloc[[0,1,2,3],[index_sample_title, index_fastq_ftp, index_read_count]]
+
+		for i in range(df.shape[0]):
+
+			sample_title = df.iloc[i,0].replace(' ','_').replace('(','').replace(')','')
+			links = df.iloc[i,1].split(';')
+
+			for link in links:
+				output = os.path.join(self.path_study, sample_title + '_' + link.rsplit('/',1)[1])
+				self.get_study(link, output)
+
+
+	def get_study(self, link, output):
+
+		try:
+			# print link
+			# print "wget -nc " + link + " --output-document= " + output + " &>/dev/null"
+			subprocess.check_output(['bash', '-c', "wget -nc " + link + " --output-document=" + output + " &>/dev/null"])
+
+		except subprocess.CalledProcessError as error:
+			print "here"
+			self.logger.warning("Error - download.py - get_study " + str(output.rsplit('/',1))[1])
+			self.logger.warning("Exception at the line : {}".format(sys.exc_info()[-1].tb_lineno))
+			self.logger.warning(sys.exc_info())
 
 if __name__ == '__main__':
 			
@@ -250,7 +279,7 @@ if __name__ == '__main__':
 	download.get_repeat_consensus()
 	download.get_repeat_feature()
 	download.get_study_info()
-	download.get_study()
+	download.get_all_study()
 
 	print "{}".format("Finished...")
 
